ed7a070f02c0f4e09e7f37d4018d3a4eae35ab78
CFL <310342136@qq.com>
2021-03-07 17:04:19 +0800

    add icons

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -12,7 +12,7 @@
 
 <script>
 export default {
-    name: "HomeHeader",
+    name: 'HomeHeader',
 };
 </script>
 
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="wrapper">
     <swiper :options="swiperOptions">
-        <swiper-slide v-for="item of swiperList" key="item"><img class="swiper-img" :src="item.imgUrl" alt=""></swiper-slide>
+        <swiper-slide v-for="item of swiperList" :key="item.id"><img class="swiper-img" :src="item.imgUrl" alt=""></swiper-slide>
         <div class="swiper-pagination" slot="pagination"></div>
     </swiper>
 </div>
