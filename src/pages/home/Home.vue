848c7997c3326d99bf1c03b475a548648fcdf482
CFL <310342136@qq.com>
2021-03-08 17:10:19 +0800

    finish page

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -15,6 +15,7 @@ import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
+import {mapState} from 'vuex'
     export default {
         name:'Home',
         data(){
@@ -22,19 +23,23 @@ import axios from 'axios'
                 swiperList:[],
                 iconList:[],
                 recommendList:[],
-                weekendList:[]
+                weekendList:[],
+                lastCity:''
             }
+        },
+        computed:{
+            ...mapState(['city'])
         },
         components:{
             HomeHeader,
             HomeSwiper,
             HomeIcons,
             HomeRecommend,
-            HomeWeekend
+            HomeWeekend,
         },
         methods:{
             getHomeInfo(){
-                axios.get('/api/index.json').then(this.getHomeInfoSucc)
+                axios.get('/api/index.json?city='+this.city).then(this.getHomeInfoSucc)
             },
             getHomeInfoSucc(res){
                 res=res.data
@@ -49,6 +54,13 @@ import axios from 'axios'
         },
         mounted(){
             this.getHomeInfo()
+            this.lastCity=this.City
+        },
+        activated(){
+            if(this.city!==this.lastCity){
+                this.lastCity=this.city
+                this.getHomeInfo()
+            }
         }
     }
 </script>
